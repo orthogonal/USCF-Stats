bc257e07cb2878a0401966f6a6ab2eab5d71bbaa
Andrew Latham <acl68@case.edu>
2012-09-22 14:37:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uscfstats::Application.config.secret_token = '36e4c0162af7e1022502943eec85c78c91e0bcd1b0d350b714a3c7b345d4b28f43ca1851af67d3832ba8a0866a27def28224e0e8feb07c902967b28ef8cb1e2f'
