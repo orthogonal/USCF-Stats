07ae1d8098766e446d95cd516d47fdcbd0f9102f
Andrew Latham <acl68@case.edu>
2012-09-22 16:38:51 -0400

    Use Nokogiri and regular expressions to parse the list of players and show name/rating pairs in a table

--- app/controllers/test_noko_controller.rb
+++ app/controllers/test_noko_controller.rb
@@ -1,10 +1,28 @@
 class TestNokoController < ApplicationController
   require 'open-uri'
-  @text = ""
+  require 'nokogiri'
+  
   def index
-    open("http://main.uschess.org/assets/msa_joomla/MbrLst.php?latham") {|f|
-        f.each_line {|line| p line}
-    }
+    doc = Nokogiri::HTML(open("http://main.uschess.org/assets/msa_joomla/MbrLst.php?latham"))
+    list = doc.at_css("pre").inner_html
+    firstNumber = list.index(/\d/)
+    list = list[firstNumber, list.length]
+    
+    # Get all the ratings by splitting on <a> tag elements and then sub-splitting on spaces.  Spaces are consistent.
+    infos = list.split(/<a[^>]*>[^<]*<\/a>/)[0...-1]    # There will be a nil element at the end because of how the USCF website is built
+    @ratings = Array.new
+    infos.each do |info| 
+      @ratings << info.split(' ')[3].strip
+    end
+
+    # Get all the names by getting just <a> tag elements and then killing off the actual tags to just get the content.
+    names = list.scan(/<a[^>]*>[^<]*<\/a>/)
+    puts names
+    @names = Array.new
+    names.each do |name|
+      @names << name.gsub(/<[^>]*>/, '').titleize.strip
+    end
+      
     render(:action => "index")
   end
 end
