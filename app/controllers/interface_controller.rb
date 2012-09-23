5cab0a23add0acb7d86ff47865870486bdcfe68c
Andrew Latham <acl68@case.edu>
2012-09-23 19:03:06 -0400

    Find everyone you've ever played and see what their ratings are now.

--- app/controllers/interface_controller.rb
+++ app/controllers/interface_controller.rb
@@ -5,12 +5,9 @@ class InterfaceController < ApplicationController
   require 'uscfwebsite'
   
   def setup
-    @names = Array.new
-    @ratings = Array.new
     @tournaments = Array.new
     @sections = Array.new
     @opponents = Array.new
-    @test = "Nope"
   end
     
   def index
@@ -19,9 +16,26 @@ class InterfaceController < ApplicationController
   end
   
   def go
-    @test = "Hello you"
+    setup()
+    id = params[:uscf_id]
+    UscfWebsite.get_tournaments(id, @tournaments, @sections)
+
+    # For each tournament, go to the URL that shows who Doc played in that tournament, and scrape up the names of the players.
+    for i in 0...(@tournaments.length)
+      url = "http://main.uschess.org/assets/msa_joomla/#{@tournaments[i].gsub("XtblMain", "XtblPlr")[0, @tournaments[i].rindex('-')]}%03d-#{id}" % @sections[i]
+      puts "URL: #{url}"
+      doc = Nokogiri::HTML(open(url))
+      links = doc.search("td:nth-child(5) a")
+      links.each do |link|
+        oppId = link.attr("href")[link.attr("href").index('?') + 1, link.attr("href").length]
+        ratings = UscfWebsite.get_actual_ratings_from_id(oppId)
+        @opponents << {:name => link.text, :id => oppId, :regular => ratings[:regular], :quick => ratings[:quick]}
+      end
+      @opponents.uniq!  # Removes duplicates in-place
+      @opponents.sort_by!{|item| -item[:regular]}
+    end
+    
     respond_to do |format|
-        format.html { render :partial => 'test', :locals => {:test => @test} }
         format.js {}
     end
   end
