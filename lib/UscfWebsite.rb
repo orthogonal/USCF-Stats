897815db8e979033b6b69c36e1224fa209d6c701
Andrew Latham <acl68@case.edu>
2012-09-30 21:12:35 -0400

    Removed obsolete method, updated readme

--- lib/UscfWebsite.rb
+++ lib/UscfWebsite.rb
@@ -219,31 +219,7 @@ class UscfWebsite
     end
     return rc
   end
-  
-  # Down here are legacy methods to keep the current demo working, they're hacked together and probably will be removed later
-  
-  def self.get_tournaments(id, tournaments, sections)
-    doc = Nokogiri::HTML(open("http://main.uschess.org/assets/msa_joomla/MbrDtlTnmtHst.php?#{id}"))
-    
-    # Figure out how many pages of tournament records Doc has
-    pages = doc.search("nobr a")
-    pages = (pages.length != 0 ? pages.length : 1)
-    
-    # Go through each page, get all the blocks with tournament information and save the name and section Doc played in.
-    for i in 1...(pages + 1)
-      if (i != 1) then doc = Nokogiri::HTML(open("http://main.uschess.org/assets/msa_joomla/MbrDtlTnmtHst.php?#{id}.#{i}")) end
-      blocks = doc.search("td:nth-child(2)")
-      blocks.each do |block|
-        link = block.at_css("a")
-        if (link != nil)
-          section = block.at_css("small")
-          tournaments << link.attributes()["href"].to_s
-          sections << section.text.scan(/\d+/).first.to_i
-        end
-      end
-    end
-  end
-  
+
   def self.get_id_from_profile_url(url)
     url = (url.class() == String ? url : url.to_s)
     return url[url.index('?') + 1, url.length]
