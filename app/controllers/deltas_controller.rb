0c0bf0fe5527a248785d641398acde445a39ae2d
Andrew Latham <acl68@case.edu>
2012-10-09 23:49:45 -0400

    User writes the player ID instead of it being a hardcoded test one

--- app/controllers/deltas_controller.rb
+++ app/controllers/deltas_controller.rb
@@ -5,11 +5,11 @@ class DeltasController < ApplicationController
   end
   
   def buildchart
-    history = UscfWebsite.get_rating_history_from_id(14433377, 0)
+    history = UscfWebsite.get_rating_history_from_id(params[:uscf_id], 0)
     @data = Array.new
     puts "HISTORY: #{history}"
     history.each do |event|
-      result = UscfWebsite.get_results_against_opponents_from_tournament(14433377, event[:id], event[:section])
+      result = UscfWebsite.get_results_against_opponents_from_tournament(params[:uscf_id], event[:id], event[:section])
       result.each do |row|
         rating = (event[:regular][:pre] != 0) ? event[:regular][:pre] : event[:regular][:post]
         @data << {:wdl => row[:wdl], :rating => rating, :delta => (row[:regular].to_i - rating)}
